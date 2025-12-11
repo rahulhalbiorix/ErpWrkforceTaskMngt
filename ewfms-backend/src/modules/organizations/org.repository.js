14a38a5e7b029df23295cce4cb97d691dc1c782c
unknown <rahulh.albiorix@gmail.com>
2025-12-11 12:42:33 +0530

    fe-add-organization-module

--- ewfms-backend/src/modules/organizations/org.repository.js
+++ ewfms-backend/src/modules/organizations/org.repository.js
@@ -1,16 +1,48 @@
 import db from '../../config/db.js';
 import { v4 as UUID } from 'uuid';
 
- class OrgRepository {
+class OrgRepository {
 
-    static async  getAllOrgs(){
-       
-        return [orgs] = await  db.query('SELECT * FROM organizations')
-    
-   }
-    
-     static async createOrg(){
-          
-     }
+  static async getAllOrgs() {
+     const [orgs] = await db.query('SELECT * FROM organization')
+     console.log('🟢🟢🟢' , orgs);
+     return orgs;
+  }
 
- }  
+  static async createOrg(organizationData) {
+
+    const { name, domain, logo_url, status } = organizationData;
+
+
+    const id = UUID();
+
+     return await db.query('INSERT INTO organization (organization_id , name , domain , logo_url , status  ) VALUES (? , ? , ? , ? , ? )', [id, name, domain, logo_url, status ]);
+     
+
+  }
+
+  static async findOrgById(id) {
+    return db.query(
+      `SELECT * FROM organization 
+             WHERE organization_id = ?`,
+      [id]
+    );
+  }
+
+  static async updateOrg(id, organizationData) {
+    const { name, domain, logo_url, status } = organizationData;
+    return db.query(
+      `UPDATE organization 
+             SET name = ?, domain = ?, logo_url = ?, status = ?, updated_at = NOW() 
+             WHERE organization_id = ?`,
+      [name, domain, logo_url, status, id]
+    );
+  }
+
+  static async deleteOrg(id) {
+    return db.query('DELETE FROM organization WHERE organization_id = ?', [id]);
+  }
+
+}  
+
+export default OrgRepository;
