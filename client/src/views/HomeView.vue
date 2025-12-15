77996d330e06ce4cb5feaf862a3af56ac839eabb
unknown <rahulh.albiorix@gmail.com>
2025-12-15 16:35:25 +0530

    fe-start-work-on-frontend

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="bg-red-200" >
+  <h1>This is a Home Page...</h1>
   </main>
 </template>
